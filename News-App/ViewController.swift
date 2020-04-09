a3cd5df0ebb5844eef0d4218c5184bcd117ed997
Oci <yoshias.gorgha@gmail.com>
2020-04-09 17:47:02 +0700

    Initial Commit

---
+++ News-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News-App
+//
+//  Created by admin on 4/9/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
